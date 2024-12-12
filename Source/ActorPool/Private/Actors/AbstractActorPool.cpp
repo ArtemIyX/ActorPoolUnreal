d3a9f70714cc95fbc551a43255a28efe8c102948
Wellsaik <artem.podorozhko@outlook.com>
2024-12-12 22:54:10 +0200

    bAlwaysRelevant = true

--- Source/ActorPool/Private/Actors/AbstractActorPool.cpp
+++ Source/ActorPool/Private/Actors/AbstractActorPool.cpp
@@ -14,6 +14,7 @@ AAbstractActorPool::AAbstractActorPool(): PoolSize(10)
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	bReplicates = true;
+	bAlwaysRelevant = true;
 }
 
 
